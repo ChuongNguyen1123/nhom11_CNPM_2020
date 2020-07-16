115df15ff3a285e32e1b6cc52e6d80ce043d4afc
THANHTINHSHR <38747559+THANHTINHSHR@users.noreply.github.com>
2020-07-16 08:25:43 +0700

    chỉnh sửa tên phương thức theo chuẩn của nhóm

--- NMCNPM/src/controller/Controller.java
+++ NMCNPM/src/controller/Controller.java
@@ -87,7 +87,7 @@ public class Controller {
 			public void actionPerformed(ActionEvent e) {
 				view.getViewListFile().setVisible(true);
 				try {
-					view.getViewListFile().setValuesList(model.getListName());
+					view.getViewListFile().setValuesList(model.getListNameTGB());
 				} catch (SQLException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
