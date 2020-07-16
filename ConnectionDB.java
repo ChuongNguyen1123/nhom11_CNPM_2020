8b615ebf464dd294abd11e8fadc89cb2bbd6ad59
ChuongNguyen1123 <51111160+ChuongNguyen1123@users.noreply.github.com>
2020-07-16 21:30:38 +0700

    Mình đã sửa lại file connect vs DB my SQL rồi các bạn tải về xóa file cũ đi bỏ file này vào là ok.

---
+++ ConnectionDB.java
@@ -0,0 +1,43 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDB {
+	public static Connection connection;
+
+	public ConnectionDB() throws ClassNotFoundException, SQLException {
+		connection = getConnection();
+
+	}
+
+	@SuppressWarnings("unused")
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		Connection con = null;
+//		---- Cau hinh connect vs SQL server ----- 
+
+//		String url = "jdbc:mysql://localhost:3306/TKB/";
+//		String user = "root";
+//		String password = "741852963";
+//		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//		Connection conn = DriverManager
+//				.getConnection("jdbc:sqlserver://localhost;database=TKB;user=sa;password=741852963;");
+		
+
+//		----------- Cau hinh connect vs mySQL -----------
+		String hostName = "localhost";
+		String dbName = "database_cnpm2020";
+		String userName = "root";
+		String password = "";
+		String url = "jdbc:mysql://" + hostName + ":3306/" + dbName
+				+ "?serverTimezone=UTC&useUnicode=true&characterEncoding=utf-8";
+		Connection conn = DriverManager.getConnection(url, userName, password);
+		System.out.println("connect success");
+		return conn;
+	}
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		new ConnectionDB().getConnection();
+	}
+}
